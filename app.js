1caac7440ad05dc5b8d1a1165a1c732290e7162b
nmusaelian-rally <nik.musaelian@broadcom.com>
2020-06-14 14:05:26 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const fetch = require('node-fetch');
+const URL   = require('url')
+require('dotenv').config()
+const config = require('./config');
+const { USERNAME, PASSWORD } = process.env
+
+const baseUrl = config['port'] ? `${config['domain']}:${config['port']}/${config['apiPath']}` : `${config['domain']}/${config['apiPath']}`
+const endpoint = 'createmeta'
+const [epic, story] = config['supportedTypes'].split(',');
+
+var headers = {
+    "Content-Type":"application/json",
+    'Authorization': 'Basic ' + Buffer.from(USERNAME + ":" + PASSWORD).toString('base64')
+}
+
+const queryURL = (issueType) => {
+    //example: http://34.105.88.232:8080/rest/api/2/issue/createmeta?projectKey=FAN&issuetypeNames=Epic&expand=projects.issuetypes.fields
+    
+    let params = {projectKey: config['projectKey'], issuetypeNames: issueType, expand: 'projects.issuetypes.fields'}
+    let url  = `${baseUrl}/${endpoint}${URL.format({ query: params })}` 
+    return url
+}
+
+const fields = async (issueType) => {
+    let url = queryURL(issueType)
+    console.log(url)
+    let response = await fetch(url, {headers: headers})
+    let data = await response.json()
+    return data["projects"][0]["issuetypes"][0]
+}
+
+const requestBody = async (issueType) => {
+    let body = {} 
+    let issueInfo = await fields(issueType)
+    console.log(issueInfo["name"])
+    if (issueInfo["name"] == 'Story'){
+        body = {"fields":{"project":{"key": "FAN"}, 
+        "summary": "Story made with Insomnia",
+        "description": "Creating a Story via REST",
+        "issuetype": {"name": "Story"}}}
+    } else if(issueInfo["name"] == 'Epic'){
+        let fields = issueInfo["fields"]
+        let keys = Object.keys(fields);
+        let cfKeys = keys.filter(key => key.toLowerCase().includes("customfield_"));
+        for (let val of cfKeys){
+            if (fields[val]["name"] == 'Epic Name'){
+                body = {"fields":{"project":{"key": "FAN"}, 
+                         [val]: "e0004",
+                         "summary": "One more epic made with Insomnia",
+                         "description": "Creating an Epic via REST",
+                         "issuetype": {"name": "Epic"}}}
+            }
+         }
+    }
+    return body
+}
+
+const createIssue = async (body = {}) => {
+    const response = await fetch(baseUrl, {
+      method: 'POST', 
+      mode: 'cors', 
+      cache: 'no-cache', 
+      credentials: 'same-origin', 
+      headers: headers,
+      body: JSON.stringify(body) 
+    });
+    return response.json(); 
+  }
+
+requestBody(epic).then(createIssue)
+//requestBody(story).then(createIssue)
+
---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    domain: 'http://34.105.88.232',
+    port: '8080',
+    projectKey: 'FAN',
+    apiPath: 'rest/api/2/issue',
+    jiraUrl: "http://0.0.0.0:5472",
+    supportedTypes: 'Epic,Story'
+  };
+  
+  module.exports = config;
